1a009896801b155dd64671461e9060c5b3fdebf3
Johnson Huang <johnsonhuang724@gmail.com>
2023-08-03 18:10:00 +0800

    [feat] add c page hot key

--- index.js
+++ index.js
@@ -55,9 +55,7 @@ class PostNode {
 
 (function () {
   console.log("bahamut hotkey loading ...");
-  let list = getPostList();
-  registerHotKey(list);
-
+  switchScript();
   console.log("bahamut hotkey loaded");
 })();
 
@@ -73,7 +71,7 @@ function getPostList() {
     linkedList.appendPost({
       title: link.innerText,
       link: link.href,
-      item : item,
+      item: item,
       index: index,
     });
   });
@@ -81,48 +79,184 @@ function getPostList() {
   return linkedList;
 }
 
-function registerHotKey(list) {
+function registerListHotKey(list) {
   document.addEventListener("keydown", function (event) {
     const keyCode = event.key;
 
     switch (keyCode) {
-      case "ArrowLeft":
-        event.preventDefault();
-        console.log("left");
-        break;
       case "ArrowRight":
         event.preventDefault();
         console.log("right");
+        jumpToPost(list.currentNode());
         break;
       case "ArrowUp":
         event.preventDefault();
         current = list.currentNode();
-        console.log(list.prev());
-        console.log(list.currentNode().data.title);
-        list.currentNode().data.item.scrollIntoView({block: "center", behavior: "smooth"});
-        toggleClass(current, list.currentNode());
+        list.prev();
+        
+        continueB(current, list);
         break;
       case "ArrowDown":
         event.preventDefault();
         current = list.currentNode();
-        console.log(list.next());
-        console.log(list.currentNode().data.title);
-        list.currentNode().data.item.scrollIntoView({block: "center", behavior: "smooth"});
-        toggleClass(current, list.currentNode());
+        list.next();
+
+        continueB(current, list);
 
         break;
     }
   });
 
+  function jumpToPost(target) {
+    window.location.hash = `#post-${target.data.index}`;
+    let jumpTarget = `${target.data.link}#post-${target.data.index}`;
+    window.location.href = jumpTarget;
+  }
+}
+
+function getPostListOnCPage() {
+  const section = document.querySelectorAll(".c-section");
+  let linkedList = new PostList();
+  section.forEach((item, index) => {
+    const id = item.id;
+    if (/^post_\d+$/.test(id)) {
+      // 在控制台输出符合条件的元素的 id
+      linkedList.appendPost({
+        id: id,
+        item: item,
+        index: index,
+      });
+    }
+  });
+
+  console.log(linkedList);
+
+  return linkedList;
+}
+
+function registerPostHotKey(list) {
+  document.addEventListener("keydown", function (event) {
+    const keyCode = event.key;
+    switch (keyCode) {
+      case "ArrowLeft":
+        event.preventDefault();
+        history.back();
+        break;
+      case "ArrowRight":
+        event.preventDefault();
+        console.log("right");
+        // jumpToPost(list.currentNode());
+        break;
+      case "ArrowUp":
+        event.preventDefault();
+        current = list.currentNode();
+        list.prev();
+        continueC(current, list);
+
+        break;
+      case "ArrowDown":
+        event.preventDefault();
+        current = list.currentNode();
+        if (current !== null && !isLoaded(current.data.item)) {
+          console.log("scrolling down");
+          window.scrollBy(0, 150);
+          return;
+        }
+        list.next();
+        continueC(current, list);
+
+        break;
+    }
+  });
+}
+
+function switchScript() {
+  if (window.location.href.startsWith("https://forum.gamer.com.tw/B.php")) {
+    let list = getPostList();
+    registerListHotKey(list);
+
+    if (window.location.hash.startsWith("#post")) {
+      let target = window.location.hash.replace("#post-", "");
+      console.log("re position to : " + target);
+      let current = list.next();
+      while (current !== null) {
+        if (current.data.index === parseInt(target)) {
+          break;
+        }
+        current = list.next();
+      }
+      console.log(current);
+
+      continueB(null, list);
+    }
+  } else if (
+    window.location.href.startsWith("https://forum.gamer.com.tw/C.php")
+  ) {
+    let list = getPostListOnCPage();
+    registerPostHotKey(list);
+  }
+}
+
+function continueB(current, list) {
+  list
+    .currentNode()
+    .data.item.scrollIntoView({ block: "center", behavior: "smooth" });
+  toggleClass(current, list.currentNode());
+
   function toggleClass(current, target) {
     if (current !== null) {
-        current.data.item.classList.remove("selected");
-        current.data.item.classList.remove("blinking");
+      current.data.item.classList.remove("selected");
+      current.data.item.classList.remove("blinking");
     }
 
     if (target !== null) {
-        target.data.item.classList.add("selected");
-        target.data.item.classList.add("blinking");
+      target.data.item.classList.add("selected");
+      target.data.item.classList.add("blinking");
     }
   }
+}
+
+function continueC(current, list) {
+  let pos = "center";
+  if (!isVisible(list.currentNode().data.item)) {
+    pos = "start";
+  }
+
+  console.log("positioning to " + pos);
+
+  list
+    .currentNode()
+    .data.item.scrollIntoView({ block: pos, behavior: "smooth" });
+  toggleClass(current, list.currentNode());
+
+  function toggleClass(current, target) {
+    if (current !== null) {
+      const post = current.data.item.querySelector(".c-post");
+      post.classList.remove("selected");
+      post.classList.remove("blinking");
+    }
+
+    if (target !== null) {
+      const post = target.data.item.querySelector(".c-post");
+      post.classList.add("selected");
+      post.classList.add("blinking");
+    }
+  }
+}
+
+function isVisible(item) {
+  const rect = item.getBoundingClientRect();
+  return (
+    rect.height <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.width <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function isLoaded(item) {
+  const rect = item.getBoundingClientRect();
+
+  return (
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
 }
